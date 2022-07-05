531d6fe2985db3ad703f3bf85a7b81b649beeb46
darloi2002 <89018927+darloi2002@users.noreply.github.com>
2022-07-05 20:30:48 +0700

    edit

---
+++ lovetrang/drivecar/magic.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    $surface = $('.surface');
+    $car = $('.car');
+    $img = $('.car img');
+    let flag = true;
+
+    const cars = ['./assets/Img_05.png', './assets/Img_06.png'];
+
+    $(document).on('click', function(e) {
+        $($surface).toggleClass('moveRight');
+        $($car).toggleClass('suspension');
+    })
+
+    $(document).on('keypress', function(e) {
+        if (e.which == 119) {
+            if (flag) {
+                flag = false;
+                $img.attr('src', cars[0]);
+            } else {
+                flag = true;
+                $img.attr('src', cars[1]);
+            }
+        }
+    })
+});
