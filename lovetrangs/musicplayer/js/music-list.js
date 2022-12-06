b58e22d5f4205153e83c4bd4b947dab13a58fa86
darloi2002 <89018927+darloi2002@users.noreply.github.com>
2022-12-06 22:01:27 +0700

    replace

---
+++ lovetrangs/musicplayer/js/music-list.js
@@ -0,0 +1,62 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Love Four",
+    artist: "Rhymastic",
+    img: "music-1",
+    src: "yeu4"
+  },
+  {
+    name: "Cho Anh",
+    artist: "Ngọc Dolil, Stilla D, VRT",
+    img: "music-2",
+    src: "choanh"
+  },
+  {
+    name: "Em Là Nhất",
+    artist: "Em nhất được chưa",
+    img: "music-3",
+    src: "emlanhat"
+  },
+  {
+    name: "Lạ Lùng",
+    artist: "Vũ",
+    img: "music-4",
+    src: "lalung"
+  },
+  {
+    name: "Lý Do Anh Xuất Hiện",
+    artist: "Tân Trần",
+    img: "music-5",
+    src: "lydoanhxuathien"
+  },
+  {
+    name: "Một Đêm Say (X)",
+    artist: "Thịnh Suy",
+    img: "music-6",
+    src: "motdemsay"
+  },
+  {
+    name: "Thương",
+    artist: "The Sheep",
+    img: "music-6",
+    src: "thuong"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
