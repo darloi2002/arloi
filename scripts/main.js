d396370fc9fd32bbc5b6401cea1bc1c75ed86a79
darloi2002 <89018927+darloi2002@users.noreply.github.com>
2023-05-20 13:08:55 +0700

    udpate

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,7 @@ document.addEventListener('contextmenu', event => event.preventDefault());
 window.addEventListener('keydown', function (e) {
 
 // if the keyCode is 16 ( shift key was pressed )
-if (e.key == 'F12') {
+if (e.key == 'F12' || e.key == 'Ctrl' || e.key == 's' || e.key == 'S') {
 
 // prevent default behaviour
 e.preventDefault();
